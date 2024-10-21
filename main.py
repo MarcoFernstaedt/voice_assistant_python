bfea9b034068b7b310dbc898b87913a33ce2b9cb
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-10-21 15:08:59 -0700

    implament check for key commands like spotify

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from assistant.stt import listen_for_hotword, listen_for_command  
 from assistant.openai_integration import get_openai_response
 from assistant.tts import speak
+from assistant.command_handler import handle_spotify_commands
 
 if __name__ == "__main__":
     if listen_for_hotword():  # Wait for the hotword
         command = listen_for_command()  # Then listen for the actual command
         if command:
-            response = get_openai_response(command)  # Send command to OpenAI
-            if response:
-                print(f"Assistant: {response}")
-                speak(response)  # Speak the response
+            if 'spotify' in command:
+                handle_spotify_commands(command)
             else:
-                print('No Valid responce from openAI')
+                response = get_openai_response(command)  # Send command to OpenAI:
+                if response:
+                    print(f"Assistant: {response}")
+                    speak(response)  # Speak the response
+                else:
+                    print('No Valid responce from openAI')
