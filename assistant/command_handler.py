333e60ea45b183f14d4e9a4df54690dc61e0bf9d
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-10-21 16:56:16 -0700

    implament logic for next and previous track

--- assistant/command_handler.py
+++ assistant/command_handler.py
@@ -1,9 +1,15 @@
-from assistant.spotify_integration import play_song
+from assistant.spotify_integration import play_song, pause_song
 # , pause_song, play_next, play_previous
 
 def handle_spotify_commands(command):
-    if 'play' in command:
-        song_name = command.replace('play', '').strip()
+    if "play" in command:
+        song_name = command.replace("play", "").strip()  # Get the song name
         play_song(song_name)
+    elif "pause" in command:
+        pause_song()
+    elif "next" in command:
+        play_next()
+    elif "previous" in command:
+        play_previous()
     else:
-        print('Spotify command not found')
+        print("Spotify command not recognized.")
