6adced8cbabffc5dec731664a831637c5c3f9802
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-10-21 16:57:40 -0700

    implament loop for app to continue to listen for commands

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from assistant.openai_integration import get_openai_response
 from assistant.tts import speak
 from assistant.command_handler import handle_spotify_commands
 
+awake = True
+
 if __name__ == "__main__":
-    if listen_for_hotword():  # Wait for the hotword
-        command = listen_for_command()  # Then listen for the actual command
-        if command:
-            if 'spotify' in command:
-                handle_spotify_commands(command)
-            else:
-                response = get_openai_response(command)  # Send command to OpenAI:
-                if response:
-                    print(f"Assistant: {response}")
-                    speak(response)  # Speak the response
+    while awake:
+        if listen_for_hotword():  # Wait for the hotword
+            command = listen_for_command()  # Then listen for the actual command
+            if command:
+                if 'stop' not in command:
+                    if 'spotify' in command:
+                        handle_spotify_commands(command)
+                    else:
+                        response = get_openai_response(command)  # Send command to OpenAI:
+                        if response:
+                            print(f"Assistant: {response}")
+                            speak(response)  # Speak the response
+                        else:
+                            print('No Valid responce from openAI')
                 else:
-                    print('No Valid responce from openAI')
+                    awake = False
+                    # break
