10ab2f6c23604f018cc1194e11d2d8bee09b6fd4
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2024-10-22 12:10:55 -0700

    handle error if device ID not found

--- assistant/spotify_integration.py
+++ assistant/spotify_integration.py
@@ -20,13 +20,35 @@ def list_devices():
             print(f"Device: {device['name']}, ID: {device['id']}")
     else:
         print("No active devices found.")
+        
+def is_device_id_valid(device_id):
+    devices = sp.devices()
+    if devices['devices']:
+        for device in devices['devices']:
+            if device[['id']] == device_id:
+                return True
+                print('Device ID is a Match!')
+    else:
+        print("No active devices found.")
 
 def play_song(song_name):
-    results = sp.search(q=song_name, type='track', limit=1)
-    if results['tracks']['items'][0]:
-        track = results['tracks']['items'][0]
-        sp.start_playback(uris=[track['uri']], device_id=os.getenv('SPOTIFY_DEVICE_ID'))
-        print(f"Playing {track['name']} by {track['artists'][0]['name']}")
+    try:
+        results = sp.search(q=song_name, type='track', limit=1)
+        if results['tracks']['items'][0]:
+            track = results['tracks']['items'][0]
+            device_id = os.getenv('SPOTIFY_DEVICE_ID')
+            if is_device_id_valid(device_id):
+                sp.start_playback(uris=[track['uri']], device_id=device_id)
+                print(f"Playing {track['name']} by {track['artists'][0]['name']}")
+            else:
+                print('No Device Found.')
+        else:
+                print(f"No results found for '{song_name}'.")
+
+    except ValueError as ve:
+        print(f"Error: {ve}")
+    except Exception as e:
+        print(f"An error occurred while trying to play the song: {e}")
         
 def pause_song():
     sp.pause_playback()
